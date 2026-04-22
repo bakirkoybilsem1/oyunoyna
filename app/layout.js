6d8896e16836efc0d0bd8a4324a5b4783ed766d9
bakirkoybilsem1 <bakirkoybilsem1@gmail.com>
2026-04-22 03:01:24 +0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'OyunOyna 🎮',
+  description: 'Çocuklar için ücretsiz online oyunlar!',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="tr">
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+    </html>
+  );
+}
